a7ba768c255e45b56df7789c78e3d3c8b766b0b1
minnakt <minna.kt@mongodb.com>
2022-08-12 10:20:44 -0400

    EVG-17441: Configure Jest

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/node_modules/",
+    "!<rootDir>/src/{main.tsx,vite-env.d.ts}",
+  ],
+  coverageReporters: ["text"],
+  moduleFileExtensions: [
+    "json",
+    "js",
+    "jsx",
+    "ts",
+    "tsx",
+  ],
+  moduleNameMapper: {
+    // "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy", // Possibly enable in EVG-17445.
+    "^uuid$": "<rootDir>/node_modules/uuid/dist/index.js",
+  },
+  modulePaths: ["<rootDir>/src"],
+  preset: "ts-jest",
+  resetMocks: true,
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+  // snapshotSerializers: ["@emotion/jest/serializer"], // Possibly enable in EVG-17445.
+  testEnvironment: "jsdom",
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
+    "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
+  ],
+  testRunner: "<rootDir>/node_modules/jest-circus/runner.js",
+  transform: {
+   "^.+\\.(js|jsx|mjs|cjs|ts|tsx)$": "<rootDir>/node_modules/ts-jest",
+  //   "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js", // Possibly enable in EVG-17445.
+   },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/',
+      //   "^.+\\.module\\.(css|sass|scss)$", // Possibly enable in EVG-17445.
+  ],
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+};
---
+++ src/test_utils/index.tsx
@@ -0,0 +1,91 @@
+import { render, queries, waitFor, within, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import type {
+  RenderResult,
+  RenderOptions,
+} from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import {
+  Route,
+  Routes,
+  // Refer to https://reactrouter.com/docs/en/v6/routers/history-router to understand
+  // why this import is marked as unstable.
+  unstable_HistoryRouter as HistoryRouter,
+} from "react-router-dom";
+import * as customQueries from "./queries";
+
+type QueriesType = typeof queries;
+type CustomQueriesType = typeof customQueries;
+type CustomRenderType = CustomQueriesType & QueriesType;
+type CustomRenderOptions = RenderOptions<CustomRenderType>;
+
+interface RenderWithRouterMatchOptions extends CustomRenderOptions {
+  route?: string;
+  history?: any;
+  path?: string;
+}
+
+// Bind our custom queries to screen.
+// https://github.com/testing-library/dom-testing-library/issues/516
+const boundQueries = within<typeof customQueries>(document.body, customQueries);
+const customScreen = { ...screen, ...boundQueries };
+
+/** 
+ * `customRender` or `render` takes an instance of react-testing-library's render method
+ *  and adds additional selectors for querying components in tests.
+ * */
+const customRender = (ui: React.ReactElement, options?: CustomRenderOptions) =>
+  render(ui, {
+    queries: { ...queries, ...customQueries },
+    ...options,
+  }) as RenderResult<CustomRenderType>;
+
+const customWithin = (ui: HTMLElement) =>
+  within(ui, { ...queries, ...customQueries });
+
+/** 
+ * `renderWithRouterMatch` implements the `customRender` method and wraps a component
+ *  with an instance of `react-router`'s `<Router />` component.
+ */
+const renderWithRouterMatch = (
+  ui: React.ReactElement,
+  options: RenderWithRouterMatchOptions = {}
+) => {
+  const {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] }),
+    path = "/",
+    ...rest
+  } = options;
+  const { rerender, ...renderRest } = customRender(
+    <HistoryRouter history={history}>
+      <Routes>
+        <Route path={path} element={ui} />
+      </Routes>
+    </HistoryRouter>,
+    rest
+  );
+  const customRerender = (element: React.ReactElement) => {
+    rerender(
+      <HistoryRouter history={history}>
+        <Routes>
+          <Route path={path} element={element} />
+        </Routes>
+      </HistoryRouter>
+    );
+  };
+  return {
+    history,
+    rerender: customRerender,
+    ...renderRest,
+  };
+};
+
+export {
+  customScreen as screen,
+  customRender as render,
+  customWithin as within,
+  renderWithRouterMatch,
+  waitFor, 
+  userEvent
+};
