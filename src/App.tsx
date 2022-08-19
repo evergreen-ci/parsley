b740e2de9dbae43c176862cb01c4d106806a6956
minnakt <minna.kt@mongodb.com>
2022-08-19 16:24:46 -0400

    Get rid of unused App code

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,7 @@
-import { useState } from "react";
 import SearchBar from "components/SearchBar";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          data-cy="app-button"
-          onClick={() => setCount((c) => c + 1)}
-          type="submit"
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <SearchBar />
-    </div>
-  );
+  return <SearchBar />;
 };
 
 export default App;
