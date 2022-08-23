00cc5f18e27cd9eb0f1e031dc68d594c812dc051
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-08-23 15:53:17 -0400

    Add initial routes for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import SearchBar from "components/SearchBar";
+import { Global, css } from "@emotion/react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavBar from "components/NavBar";
+import Content from "pages/Content";
 
-const App = () => {
-  return <SearchBar />;
-};
+const globalStyles = css`
+  background-color: white;
+  body {
+    margin: 0;
+  }
+`;
+
+const App = () => (
+  <>
+    <Global styles={globalStyles} />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route element={<Content />} path="/*" />
+      </Routes>
+    </Router>
+  </>
+);
 
 export default App;
