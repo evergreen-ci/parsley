0c36646f9a9ff6975f4133765b719290afde2cf5
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-08-29 10:57:44 -0400

    EVG-17513 Update SearchBar component to resemble designs. (#16)

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,23 +1,43 @@
 import { KeyboardEvent, useState } from "react";
 import styled from "@emotion/styled";
+import IconButton from "@leafygreen-ui/icon-button";
+import { palette } from "@leafygreen-ui/palette";
 import { Option, Select } from "@leafygreen-ui/select";
-import TextInput from "@leafygreen-ui/text-input";
+import Icon from "components/Icon";
+import IconWithTooltip from "components/IconWithTooltip";
+import TextInputWithGlyph from "components/TextInputWithGlyph";
 
+const { yellow } = palette;
 interface SearchBarProps {
   disabled?: boolean;
+  validator?: (value: string) => boolean;
+  validatorMessage?: string;
+  className?: string;
+  onSubmit?: (selected: string, value: string) => void;
 }
 
 // Basic component to test LG and Emotion integration.
-const SearchBar: React.FC<SearchBarProps> = ({ disabled = false }) => {
+const SearchBar: React.FC<SearchBarProps> = ({
+  disabled = false,
+  validator = () => true,
+  validatorMessage = "Invalid Input",
+  className,
+  onSubmit,
+}) => {
   const [input, setInput] = useState("");
   const [selected, setSelected] = useState("search");
 
+  const isValid = validator(input);
+
   const handleOnSubmit = () => {
-    setInput("");
+    if (isValid) {
+      setInput("");
+      onSubmit?.(selected, input);
+    }
   };
 
   return (
-    <Container>
+    <Container className={className}>
       <StyledSelect
         allowDeselect={false}
         aria-labelledby="searchbar-select"
@@ -37,6 +57,25 @@ const SearchBar: React.FC<SearchBarProps> = ({ disabled = false }) => {
         aria-label="searchbar-input"
         data-cy="searchbar-input"
         disabled={disabled}
+        icon={
+          isValid ? (
+            <IconButton
+              aria-label="Select plus button"
+              data-cy="searchbar-submit"
+              onClick={handleOnSubmit}
+            >
+              <Icon glyph="Plus" />
+            </IconButton>
+          ) : (
+            <IconWithTooltip
+              data-cy="searchbar-warning"
+              fill={yellow.base}
+              glyph="Warning"
+            >
+              {validatorMessage}
+            </IconWithTooltip>
+          )
+        }
         onChange={(e) => setInput(e.target.value)}
         onKeyPress={(e: KeyboardEvent<HTMLInputElement>) =>
           e.key === "Enter" && handleOnSubmit()
@@ -68,8 +107,7 @@ const StyledSelect = styled(Select)`
   }
 `;
 
-const StyledInput = styled(TextInput)`
-  width: 40vw;
+const StyledInput = styled(TextInputWithGlyph)`
   /* Remove default padding around the text input. */
   > div {
     padding: 0;
---
+++ src/components/TextInputWithGlyph/index.tsx
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+import TextInput from "@leafygreen-ui/text-input";
+import { size } from "constants/tokens";
+
+type TextInputWithGlyphProps = {
+  icon: React.ReactElement;
+} & React.ComponentProps<typeof TextInput>;
+
+const TextInputWithGlyph: React.FC<TextInputWithGlyphProps> = (props) => {
+  const { icon, ...rest } = props;
+
+  return (
+    <TextInputWrapper>
+      <TextInput {...rest} />
+      <IconWrapper>{icon}</IconWrapper>
+    </TextInputWrapper>
+  );
+};
+const TextInputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const IconWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  bottom: 0;
+  height: 36px; /* height of LG text-input */
+  position: absolute;
+  right: ${size.xxs};
+  width: ${size.l};
+  justify-content: center;
+`;
+
+export default TextInputWithGlyph;
