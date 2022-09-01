a4e00167c065742306f0c1f4bd400ea54ae0f10e
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-01 09:52:15 -0400

    typo

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         __dirname,
         "./config/leafygreen-ui/emotion.ts"
       ),
-      // CellMeasurerCache doesn't follow the same directory structurer as the rest of the react-virtualized packages
+      // CellMeasurerCache doesn't follow the same directory structure as the rest of the react-virtualized packages
       // so we need to alias it to the correct path.
       "react-virtualized/dist/es/CellMeasurerCache": path.resolve(
         "node_modules/react-virtualized/dist/es/CellMeasurer/CellMeasurerCache"
