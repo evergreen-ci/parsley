870e483de37d234a8222eefa9a8aefebc2d9a888
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-01 15:14:48 -0400

    Update cypress tests

--- cypress/integration/evergreenLogs/logView.ts
+++ cypress/integration/evergreenLogs/logView.ts
@@ -1,11 +1,36 @@
 // This file can be deleted later.
 
 describe("Basic evergreen log view", () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit("/evergreen/task_0/0/tasks");
   });
 
-  it("should be able to see the log page", () => {
-    cy.contains("EVERGREEN_TASK_LOGS").should("be.visible");
+  it("should be able to see log lines", () => {
+    cy.dataCy("log-row-0").should("be.visible");
+  });
+  it("should render ansii lines", () => {
+    cy.dataCy("ansii-row").should("be.visible");
+  });
+  it("by default should have wrapping turned off and should be able to scroll horizontally", () => {
+    cy.dataCy("log-row-2").should("be.visible");
+    cy.dataCy("log-row-2").should(
+      "contain.text",
+      "disableLogicalSessionCacheRefresh"
+    );
+    cy.dataCy("log-row-2").isNotContainedInViewport();
+    cy.get(".ReactVirtualized__Grid__innerScrollContainer").should(
+      "have.css",
+      "overflow",
+      "scroll hidden"
+    );
+  });
+  it("long lines with wrapping turned on should fit on screen", () => {
+    cy.visit("/evergreen/task_0/0/tasks?wrap=true");
+    cy.dataCy("log-row-2").should("be.visible");
+    cy.dataCy("log-row-2").should(
+      "contain.text",
+      "disableLogicalSessionCacheRefresh"
+    );
+    cy.dataCy("log-row-2").isContainedInViewport();
   });
 });
