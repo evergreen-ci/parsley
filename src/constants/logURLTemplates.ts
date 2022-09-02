d18eacdee79f4a5faa2e55f039fd6c8aadd935c6
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-02 14:19:38 -0400

    Add functions for generating urls

---
+++ src/constants/logURLTemplates.ts
@@ -0,0 +1,41 @@
+import { evergreenURL, logkeeperURL } from "utils/environmentVariables";
+import { stringifyQuery } from "utils/query-string";
+
+const getEvergreenTestLogURL = (
+  taskID: string,
+  execution: string,
+  testID: string
+) => {
+  const params = {
+    test_name: testID,
+    text: true,
+  };
+  return `${evergreenURL}/test_log/${taskID}/${execution}/?${stringifyQuery(
+    params
+  )}`;
+};
+
+const getResmokeLogURL = (buildID: string, testID: string) => {
+  const params = {
+    raw: 1,
+  };
+  return `${logkeeperURL}/build/${buildID}/test/${testID}?${stringifyQuery(
+    params
+  )}`;
+};
+
+const getEvergreenTaskLogURL = (
+  taskID: string,
+  execution: string,
+  origin: string
+) => {
+  const params = {
+    text: true,
+    type: origin,
+  };
+  return `${evergreenURL}/task_log_raw/${taskID}/${execution}?${stringifyQuery(
+    params
+  )}`;
+};
+
+export { getEvergreenTestLogURL, getResmokeLogURL, getEvergreenTaskLogURL };
