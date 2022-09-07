d06c9a81465249161a402b0a57e0db2ca5e89c14
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-07 13:57:08 -0400

    Export slugs as an enum

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -5,19 +5,20 @@ const paths = {
   resmokeLogs: "/resmoke",
 };
 
-const slugs = {
-  taskID: ":task_id",
-  execution: ":execution",
-  origin: ":origin",
-  testID: ":test_id",
-  buildID: ":build_id",
-};
+enum slugs {
+  taskID = "task_id",
+  execution = "execution",
+  origin = "origin",
+  testID = "test_id",
+  buildID = "build_id",
+}
 
 const routes = {
   root: paths.home,
-  evergreenLogs: `${paths.evergreenLogs}/${slugs.taskID}/${slugs.execution}/${slugs.origin}`,
-  testLogs: `${paths.testLogs}/${slugs.taskID}/${slugs.execution}/${slugs.testID}`,
-  resmokeLogs: `${paths.resmokeLogs}/${slugs.buildID}/${slugs.testID}`,
+  evergreenLogs: `${paths.evergreenLogs}/:${slugs.taskID}/:${slugs.execution}/:${slugs.origin}`,
+  testLogs: `${paths.testLogs}/:${slugs.taskID}/:${slugs.execution}/:${slugs.testID}`,
+  resmokeLogs: `${paths.resmokeLogs}/:${slugs.buildID}/:${slugs.testID}`,
 };
 
+export { slugs };
 export default routes;
