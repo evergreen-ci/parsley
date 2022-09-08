4ff0cd7b5ce957070c007f0fd41e9f148b173b5f
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-08 16:24:20 -0400

    Add confirmation for navigating away from logs page

---
+++ src/components/NavBar/UploadLink/index.tsx
@@ -0,0 +1,60 @@
+import { useCallback, useState } from "react";
+import { css } from "@emotion/react";
+import ConfirmationModal from "@leafygreen-ui/confirmation-modal";
+import { H3 } from "@leafygreen-ui/typography";
+import { useNavigate } from "react-router-dom";
+import { StyledRouterLink } from "components/styles";
+import routes from "constants/routes";
+import { size, zIndex } from "constants/tokens";
+
+interface UploadLinkProps {
+  hasLogs: boolean;
+  clearLogs: () => void;
+}
+const UploadLink: React.FC<UploadLinkProps> = ({ hasLogs, clearLogs }) => {
+  const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
+  const handleClick = useCallback(() => {
+    if (hasLogs) {
+      setOpen(true);
+    } else {
+      navigate(routes.upload);
+    }
+  }, [hasLogs, navigate]);
+  return (
+    <>
+      <StyledRouterLink
+        css={navLinkStyles}
+        data-cy="upload-link"
+        onClick={handleClick}
+        to={hasLogs ? "#" : routes.upload}
+      >
+        Upload
+      </StyledRouterLink>
+      <ConfirmationModal
+        buttonText="Confirm"
+        css={css`
+          z-index: ${zIndex.modal};
+        `}
+        data-cy="confirmation-modal"
+        onCancel={() => setOpen(false)}
+        onConfirm={() => {
+          clearLogs();
+          setOpen(false);
+          navigate(routes.upload);
+        }}
+        open={open}
+        title="Navigating away will clear your current logs."
+        variant="danger"
+      >
+        <H3>Are you sure you want to navigate away?</H3>
+      </ConfirmationModal>
+    </>
+  );
+};
+
+const navLinkStyles = css`
+  margin: 0 ${size.s};
+`;
+
+export default UploadLink;
