e93a741cb27ff4e55ff5b875dc35271627e971ce
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-09 15:05:19 -0400

    Support ansii rendered lines

--- src/components/LogRow/AnsiiRow/index.tsx
+++ src/components/LogRow/AnsiiRow/index.tsx
@@ -1,6 +1,11 @@
-import { forwardRef } from "react";
+import { forwardRef, useMemo } from "react";
+import AnsiUp from "ansi_up";
+import parse from "html-react-parser";
+import linkifyHtml from "linkify-html";
 import BaseRow from "components/LogRow/BaseRow";
 import { BaseRowProps } from "../types";
+
+const ansiUp = new AnsiUp();
 
 const AnsiiRow = forwardRef<any, BaseRowProps>((rowProps, ref) => {
   const { data, listRowProps } = rowProps;
@@ -11,9 +16,20 @@ const AnsiiRow = forwardRef<any, BaseRowProps>((rowProps, ref) => {
   // TODO: EVG-17535
   // This should be replaced with a collapsible component
   const lineIndex = Array.isArray(line) ? line[0] : line;
+  const lineContent = getLine(lineIndex);
+  const memoizedLogLine = useMemo(() => {
+    if (!lineContent) return null;
+    const render = linkifyHtml(ansiUp.ansi_to_html(lineContent), {
+      validate: {
+        url: (value: string) => /^(http)s?:\/\//.test(value),
+      },
+    });
+    return parse(render);
+  }, [lineContent]);
+  if (!lineContent) return null;
   return (
     <BaseRow wrap={wrap} {...listRowProps} ref={ref}>
-      <span data-cy="ansii-row">{getLine(lineIndex)}</span>
+      <span data-cy="ansii-row">{memoizedLogLine}</span>
     </BaseRow>
   );
 });
