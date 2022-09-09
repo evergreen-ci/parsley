f080f1355925d3a80bd4000fbcfb190a08fed0b1
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-09 13:51:39 -0400

    Slight perf optimizations

--- src/hooks/useAxios/index.ts
+++ src/hooks/useAxios/index.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useAxiosGet = (url: string) => {
@@ -6,28 +6,30 @@ const useAxiosGet = (url: string) => {
   const [data, setData] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
-  axios
-    .get(url, {
-      withCredentials: true,
-      maxBodyLength: Infinity,
-      maxContentLength: Infinity,
-      onDownloadProgress: (progressEvent) => {
-        const percentCompleted = Math.round(
-          (progressEvent.loaded * 100) / progressEvent.total
-        );
-        setDownloadProgress(percentCompleted);
-      },
-      responseType: "text",
-    })
-    .then(({ data: d }) => {
-      setIsLoading(false);
-      setData(d);
-    })
-    .catch((e) => {
-      setIsLoading(false);
-      setError(e);
-    });
-
+  useEffect(() => {
+    axios
+      .get(url, {
+        withCredentials: true,
+        maxBodyLength: Infinity,
+        maxContentLength: Infinity,
+        onDownloadProgress: (progressEvent) => {
+          const percentCompleted = Math.round(
+            (progressEvent.loaded * 100) / progressEvent.total
+          );
+          setDownloadProgress(percentCompleted);
+        },
+        responseType: "text",
+      })
+      .then(({ data: d }) => {
+        setIsLoading(false);
+        setData(d);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return { data, error, isLoading, downloadProgress };
 };
 
