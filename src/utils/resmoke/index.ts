81c993402f370af134e766d363d291ac1f69b5ab
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-11 14:54:34 -0400

    comment lines

--- src/utils/resmoke/index.ts
+++ src/utils/resmoke/index.ts
@@ -1,8 +1,8 @@
 const processResmokeLine = (line: string) => {
-  const port = line.match(portRegex)?.[0];
+  const port = line.match(portRegex)?.[1];
   const resmokeFunction = line.match(resmokeFunctionRegex)?.[0];
-  const json = line.match(jsonRegex)?.[0] || "";
-  if (!resmokeFunction || !json) {
+  const json = line.match(jsonRegex)?.[1] || "";
+  if (!resmokeFunction && !json) {
     // Its not a resmoke line with json so we can just return it
     return line;
   }
@@ -61,18 +61,73 @@ const conditionallyBuildResmokeLine = (
   return line;
 };
 
+/** sdbc following by a 5 digit number
+ * @example s12345
+ * @example d54321
+ */
 const portRegex = / [sdbc](\d{1,5})/;
-const resmokeFunctionRegex = /\[.*\]/gm;
+
+/** The resmoke function
+ * @example [js:test]
+ * @example [js:setup]
+ */
+const resmokeFunctionRegex = /\[.*\]/;
+
+/** The json object
+ * @example {"t":{"$date":"2021-03-03T20:54:54.000Z"},"s":"I",  "c":"NETWORK",  "id":22900,  "ctx":"conn1","msg":"client metadata","attr":{"remote":"
+ */
 const jsonRegex = /\{(.*)\}/;
+
+/** The timestamp
+ * @example "t":{"$date":"2021-03-03T20:54:54.000Z"}
+ * @example "t":{"$date":"2021-03-03T20:54:54.000Z"}
+ */
 const timeStampRegex =
   /\{"t":\{"\$date":"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z)"\}/;
+
+/** The timestamp with offset
+ * @example "t":{"$date":"2021-03-03T20:54:54.000-0400"}
+ * @example "t":{"$date":"2021-03-03T20:54:54.000+0400"}
+ */
 const timestampWithOffsetRegex =
   /{"t":{"\$date":"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}\+\d{2}:\d{2})"}/;
+
+/** The shell prefix
+ * @example "s":"I"
+ * @example "s":"W"
+ */
 const shellPrefixRegex = /"s":"([A-z])"/;
+
+/** The config server
+ * @example "c":"NETWORK"
+ * @example "c":"STORAGE"
+ * @example "c":"COMMAND"
+ * @example "c":"REPL"
+ */
 const configSrvRegex = /"c":"([A-z]*)"/;
+
+/** The id
+ * @example "id":22900
+ * @example "id":22901
+ * @example "id":22902
+ */
 const idRegex = /"id":([0-9]*)/;
+
+/** The context
+ * @example "ctx":"conn1"
+ * @example "ctx":"conn2"
+ * @example "ctx":"conn3"
+ */
 const ctxRegex = /"ctx":"([A-z0-9]*)"/;
+
+/** The message
+ * @example "msg":"client metadata"
+ */
 const msgRegex = /msg":"(.*?)"/;
+
+/** The attributes
+ * @example "attr":{"remote":"
+ */
 const attrRegex = /("attr":.*)}/;
 
 export { processResmokeLine };
