5d2269e8245499a286c84edc041a6383eaf94ec3
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-12 12:04:05 -0400

    EVG-15938 Add File uploader (#31)

--- src/components/LogWindow/index.tsx
+++ src/components/LogWindow/index.tsx
@@ -12,9 +12,10 @@ import { useQueryParam } from "hooks/useQueryParam";
 
 interface LogWindowProps {
   logType: LogTypes;
+  isUploadedLog: boolean;
 }
-const LogWindow: React.FC<LogWindowProps> = ({ logType }) => {
-  const { logLines, getLine } = useLogContext();
+const LogWindow: React.FC<LogWindowProps> = ({ logType, isUploadedLog }) => {
+  const { logLines, hasLogs, getLine } = useLogContext();
   const [wrap] = useQueryParam(QueryParams.Wrap, false);
   const [scrollToIndex] = useQueryParam(QueryParams.SelectedLine, 0);
 
@@ -26,10 +27,10 @@ const LogWindow: React.FC<LogWindowProps> = ({ logType }) => {
   );
 
   return (
-    <Container>
-      {logLines.length !== 0 && <SideBar maxLineNumber={logLines.length - 1} />}
+    <Container data-cy="log-window">
+      {hasLogs && <SideBar maxLineNumber={logLines.length - 1} />}
       <ColumnContainer>
-        <SubHeader />
+        <SubHeader isUploadedLog={isUploadedLog} />
         <LogPaneContainer>
           <LogPane
             cache={cache}
