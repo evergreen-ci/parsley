644a5ad6788ab4306f887293539d1d80b9d12659
Mohamed Khelif <khelif96@gmail.com>
2022-09-13 09:44:45 -0400

    EVG-17524 Add ANSII encoded log line support (#36)

--- src/components/LogRow/AnsiiRow/index.tsx
+++ src/components/LogRow/AnsiiRow/index.tsx
@@ -1,6 +1,11 @@
-import { forwardRef } from "react";
+import { forwardRef, useMemo } from "react";
+import AnsiUp from "ansi_up";
+import parse from "html-react-parser";
+import linkifyHtml from "linkify-html";
 import BaseRow from "components/LogRow/BaseRow";
 import { BaseRowProps } from "../types";
+
+const ansiUp = new AnsiUp();
 
 const AnsiiRow = forwardRef<any, BaseRowProps>((rowProps, ref) => {
   const { data, listRowProps } = rowProps;
@@ -11,12 +16,31 @@ const AnsiiRow = forwardRef<any, BaseRowProps>((rowProps, ref) => {
   // TODO: EVG-17535
   // This should be replaced with a collapsible component
   const lineIndex = Array.isArray(line) ? line[0] : line;
+  const lineContent = getLine(lineIndex);
+  if (!lineContent) return null;
   return (
     <BaseRow wrap={wrap} {...listRowProps} ref={ref}>
-      <span data-cy="ansii-row">{getLine(lineIndex)}</span>
+      <ProcessedAnsiiRow lineContent={lineContent} />
     </BaseRow>
   );
 });
+
+interface ProcessedAnsiiRowProps {
+  lineContent: string;
+}
+const ProcessedAnsiiRow: React.FC<ProcessedAnsiiRowProps> = ({
+  lineContent,
+}) => {
+  const memoizedLogLine = useMemo(() => {
+    const render = linkifyHtml(ansiUp.ansi_to_html(lineContent), {
+      validate: {
+        url: (value: string) => /^(http)s?:\/\//.test(value),
+      },
+    });
+    return parse(render);
+  }, [lineContent]);
+  return <span data-cy="ansii-row">{memoizedLogLine}</span>;
+};
 
 AnsiiRow.displayName = "AnsiiRow";
 
