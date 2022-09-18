22b2cdf5f8d4fc14031f95e53b4e3d2ddd131e08
minnakt <minna.kt@mongodb.com>
2022-09-17 22:44:35 -0400

    Add error state to search range

--- src/components/DetailsOverlay/SearchRangeInput/index.tsx
+++ src/components/DetailsOverlay/SearchRangeInput/index.tsx
@@ -1,9 +1,12 @@
 import styled from "@emotion/styled";
+import { palette } from "@leafygreen-ui/palette";
 import TextInput from "@leafygreen-ui/text-input";
 import { QueryParams } from "constants/queryParams";
 import { size } from "constants/tokens";
 import { useQueryParam } from "hooks/useQueryParam";
 import { FilterRow, StyledSubtitle } from "../styles";
+
+const { red } = palette;
 
 const SearchRangeInput: React.FC = () => {
   const [lowerBound, setLowerQueryParam] = useQueryParam<number | undefined>(
@@ -14,6 +17,11 @@ const SearchRangeInput: React.FC = () => {
     QueryParams.UpperRange,
     undefined
   );
+
+  const hasError =
+    lowerBound !== undefined && upperBound !== undefined
+      ? lowerBound > upperBound
+      : false;
 
   const updateLowerBound = (newVal: string) => {
     if (newVal === "") {
@@ -32,40 +40,64 @@ const SearchRangeInput: React.FC = () => {
   };
 
   return (
-    <FilterRow>
+    <StyledFilterRow>
       <StyledSubtitle>Range</StyledSubtitle>
+
       <RangeContainer>
-        <RangeInput
-          aria-labelledby="Range Lower Bound"
-          data-cy="range-lower-bound"
-          min={0}
-          onChange={(e) => updateLowerBound(e.target.value)}
-          placeholder="0"
-          sizeVariant="small"
-          type="number"
-          value={(lowerBound ?? "").toString()}
-        />
-        <RangeInput
-          aria-labelledby="Range Upper Bound"
-          data-cy="range-upper-bound"
-          min={-1}
-          onChange={(e) => updateUpperBound(e.target.value)}
-          placeholder="100"
-          sizeVariant="small"
-          type="number"
-          value={(upperBound ?? "").toString()}
-        />
+        <InputContainer>
+          <RangeInput
+            aria-labelledby="Range Lower Bound"
+            data-cy="range-lower-bound"
+            min={0}
+            onChange={(e) => updateLowerBound(e.target.value)}
+            placeholder="0"
+            sizeVariant="small"
+            state={hasError ? "error" : "none"}
+            type="number"
+            value={(lowerBound ?? "").toString()}
+          />
+          <RangeInput
+            aria-labelledby="Range Upper Bound"
+            data-cy="range-upper-bound"
+            min={0}
+            onChange={(e) => updateUpperBound(e.target.value)}
+            placeholder="100"
+            sizeVariant="small"
+            state={hasError ? "error" : "none"}
+            type="number"
+            value={(upperBound ?? "").toString()}
+          />
+        </InputContainer>
+        {hasError && (
+          <WarningMessage>Specified range is not valid.</WarningMessage>
+        )}
       </RangeContainer>
-    </FilterRow>
+    </StyledFilterRow>
   );
 };
 
+const StyledFilterRow = styled(FilterRow)`
+  align-items: flex-start;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const RangeContainer = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 
 const RangeInput = styled(TextInput)`
   width: ${size.xxl};
+  margin-left: ${size.xs};
+`;
+
+const WarningMessage = styled.div`
+  color: ${red.base};
+  margin-top: ${size.xxs};
   margin-left: ${size.xs};
 `;
 
