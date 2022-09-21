625bcfefae6b33e20eaf261f108337aa83ccd519
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-21 16:58:38 -0400

    EVG-17539 Resmoke line processing (#39)

--- src/context/LogContext/LogContext.test.tsx
+++ src/context/LogContext/LogContext.test.tsx
@@ -1,4 +1,5 @@
 import { act, renderHook } from "@testing-library/react-hooks";
+import { LogTypes } from "constants/enums";
 import { LogContextProvider, useLogContext } from ".";
 
 describe("useLogContext", () => {
@@ -16,10 +17,29 @@ describe("useLogContext", () => {
     );
     const { result } = renderHook(() => useLogContext(), { wrapper });
     act(() => {
-      result.current.ingestLines(["foo", "bar"]);
+      result.current.ingestLines(["foo", "bar"], LogTypes.EVERGREEN_TASK_LOGS);
     });
     expect(result.current.logLines).toStrictEqual(["foo", "bar"]);
     expect(result.current.lineCount).toBe(2);
+  });
+  it("ingesting a resmoke log should transform it before adding it to the list of logs", () => {
+    const wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+      <LogContextProvider>{children}</LogContextProvider>
+    );
+    const { result } = renderHook(() => useLogContext(), { wrapper });
+    act(() => {
+      result.current.ingestLines(
+        [
+          `[j0:s0:n1] {"t":{"$date":"2022-09-13T16:57:46.852+00:00"},"s":"D2", "c":"REPL_HB",  "id":4615670, "ctx":"ReplCoord-1","msg":"Sending heartbeat","attr":{"requestId":3705,"target":"localhost:20003","heartbeatObj":{"replSetHeartbeat":"shard-rs0","configVersion":5,"configTerm":3,"hbv":1,"from":"localhost:20004","fromId":1,"term":3,"primaryId":1}}}`,
+          `[j0:s0] {"t":{"$date":"2022-09-13T16:57:46.855+00:00"},"s":"I",  "c":"-",        "id":20883,   "ctx":"conn188","msg":"Interrupted operation as its client disconnected","attr":{"opId":6047}}`,
+        ],
+        LogTypes.RESMOKE_LOGS
+      );
+    });
+    expect(result.current.logLines).toStrictEqual([
+      `[j0:s0:n1] | 2022-09-13T16:57:46.852+00:00 D2 REPL_HB  4615670 [ReplCoord-1] "Sending heartbeat","attr":{"requestId":3705,"target":"localhost:20003","heartbeatObj":{"replSetHeartbeat":"shard-rs0","configVersion":5,"configTerm":3,"hbv":1,"from":"localhost:20004","fromId":1,"term":3,"primaryId":1}}`,
+      `[j0:s0] | 2022-09-13T16:57:46.855+00:00 I  -        20883   [conn188] "Interrupted operation as its client disconnected","attr":{"opId":6047}`,
+    ]);
   });
   it("saving a filename should save it to the context", () => {
     const wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
--- src/context/LogContext/state.ts
+++ src/context/LogContext/state.ts
@@ -1,12 +1,15 @@
 import { useReducer } from "react";
+import { LogTypes } from "constants/enums";
+import { processResmokeLine } from "utils/resmoke";
 
 interface LogState {
   logs: string[];
   fileName?: string;
+  logType?: LogTypes;
 }
 
 type Action =
-  | { type: "INGEST_LOGS"; logs: string[] }
+  | { type: "INGEST_LOGS"; logs: string[]; logType: LogTypes }
   | { type: "CLEAR_LOGS" }
   | { type: "SET_FILE_NAME"; fileName: string };
 
@@ -14,18 +17,29 @@ const initialState = (initialLogLines?: string[]): LogState => ({
   logs: initialLogLines || [],
 });
 
-const reducer = (state: LogState, action: Action) => {
+const reducer = (state: LogState, action: Action): LogState => {
   switch (action.type) {
-    case "INGEST_LOGS":
+    case "INGEST_LOGS": {
+      let processedLogs = action.logs;
+      switch (action.logType) {
+        case LogTypes.RESMOKE_LOGS:
+          processedLogs = action.logs.map(processResmokeLine);
+          break;
+        default:
+          break;
+      }
       return {
         ...state,
-        logs: action.logs,
+        logs: processedLogs,
+        logType: action.logType,
       };
+    }
     case "CLEAR_LOGS":
       return {
         ...state,
         logs: [],
-        fileName: undefined,
+        fileName: "",
+        logType: undefined,
       };
     case "SET_FILE_NAME":
       return {
