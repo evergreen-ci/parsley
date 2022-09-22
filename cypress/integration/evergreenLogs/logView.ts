09e7e156963cd955ffe8aa942ee16cb72ec24afc
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-22 19:14:18 -0400

    Finish evergreen tests

--- cypress/integration/evergreenLogs/logView.ts
+++ cypress/integration/evergreenLogs/logView.ts
@@ -35,7 +35,7 @@ describe("Basic evergreen log view", () => {
     cy.dataCy("log-row-2").should("be.visible");
     cy.dataCy("log-row-2").should(
       "contain.text",
-      `[2022/03/02 17:02:18.500] warning Pattern ["@apollo/client@latest"] is trying to unpack in the same destination "/home/ubuntu/.cache/yarn/v6/npm-@apollo-client-3.3.7-f15bf961dc0c2bee37a47bf86b8881fdc6183810-integrity/node_modules/@apollo/client" as pattern ["@apollo/client@3.3.7"]. This could result in non-deterministic behavior, skipping.`
+      `[2022/03/02 17:01:58.701] Running pre-task commands.`
     );
     cy.dataCy("log-row-22").isContainedInViewport();
   });
@@ -48,17 +48,17 @@ describe("Bookmarking and selecting lines", () => {
   });
 
   it("should default to bookmarking 0 and the last log line on load", () => {
-    cy.location("search").should("equal", "?bookmarks=0,1192");
+    cy.location("search").should("equal", "?bookmarks=0,1788");
     cy.dataCy("log-line-container").should("contain", "0");
-    cy.dataCy("log-line-container").should("contain", "1192");
+    cy.dataCy("log-line-container").should("contain", "1788");
   });
 
   it("should be able to bookmark and unbookmark log lines", () => {
     cy.dataCy("log-row-4").dblclick();
-    cy.location("search").should("equal", "?bookmarks=0,4,1192");
+    cy.location("search").should("equal", "?bookmarks=0,4,1788");
     cy.dataCy("log-line-container").should("contain", "0");
     cy.dataCy("log-line-container").should("contain", "4");
-    cy.dataCy("log-line-container").should("contain", "1192");
+    cy.dataCy("log-line-container").should("contain", "1788");
 
     cy.dataCy("log-row-4").dblclick();
     cy.dataCy("log-line-container").should("not.contain", "4");
@@ -66,11 +66,11 @@ describe("Bookmarking and selecting lines", () => {
 
   it("should be able to select and unselect lines", () => {
     cy.dataCy("log-link-5").click();
-    cy.location("search").should("equal", "?bookmarks=0,1192&selectedLine=5");
+    cy.location("search").should("equal", "?bookmarks=0,1788&selectedLine=5");
     cy.dataCy("log-line-container").should("contain", "5");
 
     cy.dataCy("log-link-5").click();
-    cy.location("search").should("equal", "?bookmarks=0,1192");
+    cy.location("search").should("equal", "?bookmarks=0,1788");
     cy.dataCy("log-line-container").should("not.contain", "5");
   });
 
@@ -86,18 +86,14 @@ describe("Filtering", () => {
     cy.dataCy("filter-option").click();
     cy.dataCy("searchbar-input").type("starting{enter}");
 
-    cy.dataCy("log-row-0").should("be.visible");
-    cy.dataCy("log-row-1").should("be.visible");
-    cy.dataCy("log-row-2").should("not.exist");
-    cy.dataCy("log-row-3").should("not.exist");
-    cy.dataCy("log-row-4").should("be.visible");
-    cy.dataCy("log-row-5").should("not.exist");
-    cy.dataCy("log-row-6").should("not.exist");
-    cy.dataCy("log-row-7").should("be.visible");
+    cy.get("[data-cy^='log-row-']").each(($el) => {
+      cy.wrap($el).contains("starting", { matchCase: false });
+    });
   });
 
   it("should respect applied filters and selected lines", () => {
     // TODO EVG-17908: Instead of revisiting the page, delete the filters from the drawer.
+    cy.login(); // TODO EVG-17908: Remove this line.
     cy.visit(logLink);
 
     // Select a line, with the expectation that it won't be collapsed by the filter.
@@ -109,13 +105,11 @@ describe("Filtering", () => {
     cy.dataCy("filter-option").click();
     cy.dataCy("searchbar-input").type("notarealfilter{enter}");
 
-    cy.dataCy("log-row-0").should("be.visible");
-    cy.dataCy("log-row-1").should("not.exist");
-    cy.dataCy("log-row-2").should("not.exist");
-    cy.dataCy("log-row-3").should("not.exist");
-    cy.dataCy("log-row-4").should("not.exist");
-    cy.dataCy("log-row-5").should("be.visible");
-    cy.dataCy("log-row-6").should("be.visible");
-    cy.dataCy("log-row-7").should("be.visible");
+    cy.get("[data-cy^='log-row-']").each(($el) => {
+      // Matched elements should be one of the bookmarked or selected values
+      cy.wrap($el)
+        .should("have.attr", "data-cy")
+        .and("match", /log-row-(0|5|6)/);
+    });
   });
 });
