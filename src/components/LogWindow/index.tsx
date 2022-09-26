696f1f4dc9a1c06b14f7a1c4a4bdd1cb071b0ad4
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-26 17:56:52 -0400

    Move log Parsing to context

--- src/components/LogWindow/index.tsx
+++ src/components/LogWindow/index.tsx
@@ -1,40 +1,27 @@
-import { useMemo } from "react";
 import styled from "@emotion/styled";
 import LogPane from "components/LogPane";
 import { RowRenderer, cache } from "components/LogRow/RowRenderer";
 import SideBar from "components/SideBar";
 import SubHeader from "components/SubHeader";
 import { LogTypes } from "constants/enums";
-import { FilterLogic, QueryParams } from "constants/queryParams";
+import { QueryParams } from "constants/queryParams";
 import { navbarHeight, size, subheaderHeight } from "constants/tokens";
 import { useLogContext } from "context/LogContext";
 import { useQueryParam } from "hooks/useQueryParam";
-import { filterLogs } from "utils/filter";
 
 interface LogWindowProps {
   logType: LogTypes;
   isUploadedLog: boolean;
 }
 const LogWindow: React.FC<LogWindowProps> = ({ logType, isUploadedLog }) => {
-  const { logLines, hasLogs, getLine } = useLogContext();
+  const { hasLogs, getLine, lineCount, processedLogLines, scrollToLine } =
+    useLogContext();
   const [wrap] = useQueryParam(QueryParams.Wrap, false);
   const [filters] = useQueryParam<string[]>(QueryParams.Filters, []);
-  const [bookmarks] = useQueryParam<number[]>(QueryParams.Bookmarks, []);
-  const [selectedLine] = useQueryParam<number | undefined>(
-    QueryParams.SelectedLine,
-    undefined
-  );
-  const [filterLogic] = useQueryParam(QueryParams.FilterLogic, FilterLogic.And);
-
-  // TODO EVG-17537: more advanced filtering
-  const processedLogLines = useMemo(
-    () => filterLogs(logLines, filters, bookmarks, selectedLine, filterLogic),
-    [logLines, filters, bookmarks, selectedLine, filterLogic]
-  );
 
   return (
     <Container data-cy="log-window">
-      {hasLogs && <SideBar maxLineNumber={logLines.length - 1} />}
+      {hasLogs && <SideBar maxLineNumber={lineCount - 1} />}
       <ColumnContainer>
         <SubHeader isUploadedLog={isUploadedLog} />
         <LogPaneContainer>
@@ -49,7 +36,7 @@ const LogWindow: React.FC<LogWindowProps> = ({ logType, isUploadedLog }) => {
               getLine,
               processedLines: processedLogLines,
             })}
-            scrollToIndex={selectedLine}
+            scrollToIndex={scrollToLine}
             wrap={wrap}
           />
         </LogPaneContainer>
