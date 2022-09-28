462bcd9b34792a5f64dcb2fdaeae1b2f8c932a7b
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-09-28 11:54:18 -0400

    Move mock

--- src/hooks/useAxios/useAxios.test.ts
+++ src/hooks/useAxios/useAxios.test.ts
@@ -15,8 +15,8 @@ const mockApi = {
 jest.mock("axios");
 
 describe("useAxiosGet", () => {
+  const mockedAxiosGet = axios.get as jest.MockedFunction<typeof axios.get>;
   it("gets a good response from the api and updates its state", async () => {
-    const mockedAxiosGet = axios.get as jest.MockedFunction<typeof axios.get>;
     mockedAxiosGet.mockResolvedValue(mockApi);
     const { result, waitForNextUpdate } = renderHook(() =>
       useAxiosGet(API_URL)
@@ -27,7 +27,6 @@ describe("useAxiosGet", () => {
     expect(result.current.data).toBe(jsonMessage);
   });
   it("gets a bad response from the api and returns an error", async () => {
-    const mockedAxiosGet = axios.get as jest.MockedFunction<typeof axios.get>;
     mockedAxiosGet.mockRejectedValue({ status: 404, message: "error" });
     const { result, waitForNextUpdate } = renderHook(() =>
       useAxiosGet(API_URL)
