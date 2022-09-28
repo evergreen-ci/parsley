89965b0e828f13015b2f9cdb6949a920cab233e8
Mohamed Khelif <khelif96@gmail.com>
2022-09-28 11:47:22 -0400

    Update scripts/bootstrap-logkeeper.sh
    Co-authored-by: minnakt <47064971+minnakt@users.noreply.github.com>

--- scripts/bootstrap-logkeeper.sh
+++ scripts/bootstrap-logkeeper.sh
@@ -44,7 +44,7 @@ if [ ! -d "bin/_bucketdata" ]; then
     
 else
     echo "Found bin/_bucketdata directory, skipping download..."
-    echo "If you want to download the bucket data again, delete the _bucketdata directory and run this script again."
+    echo "If you want to download the bucket data again, delete the _bucketdata directory and rerun this script."
 fi
 
 
