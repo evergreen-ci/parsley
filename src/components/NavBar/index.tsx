5bc74529a8ff29651997ddc6235cb929e421b603
minnakt <minna.kt@mongodb.com>
2022-10-03 18:09:24 -0400

    Disable navbar interactions if there are no logs

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -39,13 +39,18 @@ const NavBar: React.FC = () => {
           <UploadLink clearLogs={clearLogs} hasLogs={hasLogs} />
         </LinkContainer>
         <StyledSearchBar
+          disabled={!hasLogs}
           onSubmit={handleSearch}
           validator={validateRegexp}
           validatorMessage="Invalid Regular Expression"
         />
       </FlexContainer>
 
-      <StyledButton buttonText="Details" data-cy="details-button">
+      <StyledButton
+        buttonText="Details"
+        data-cy="details-button"
+        disabled={!hasLogs}
+      >
         <DetailsOverlay />
       </StyledButton>
     </Container>
