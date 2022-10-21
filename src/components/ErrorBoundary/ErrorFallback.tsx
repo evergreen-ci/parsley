3115818a875a79a5a08e6ec9a9e26e97cc84d9c6
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-10-21 14:39:02 -0400

    Add ErrorBoundary tests and stories

--- src/components/ErrorBoundary/ErrorFallback.tsx
+++ src/components/ErrorBoundary/ErrorFallback.tsx
@@ -2,16 +2,15 @@ import styled from "@emotion/styled";
 import { palette } from "@leafygreen-ui/palette";
 import { H1, H2 } from "@leafygreen-ui/typography";
 import { size } from "constants/tokens";
+import { H1Type, H2Type } from "types/leafygreen";
 import errorPage from "./errorPage.svg";
 
 const { white } = palette;
 
 const ErrorFallback = () => (
-  <Center>
+  <div>
     <Text>
-      {/* @ts-expect-error */}
       <StyledHeader>Error</StyledHeader>
-      {/* @ts-expect-error */}
       <StyledSubtitle>
         Ouch! That&apos;s gotta hurt,
         <br /> sorry about that!
@@ -19,34 +18,28 @@ const ErrorFallback = () => (
       <StyledLink href="/">Back To Home</StyledLink>
     </Text>
     <img alt="Error Background" src={errorPage} />
-  </Center>
+  </div>
 );
 
 export default ErrorFallback;
 
-// @ts-expect-error
-const StyledHeader = styled(H1)`
+const StyledHeader = styled<H1Type>(H1)`
   color: ${white};
 `;
-// @ts-expect-error
-const StyledSubtitle = styled(H2)`
+
+const StyledSubtitle = styled<H2Type>(H2)`
   color: ${white};
 `;
+
 const StyledLink = styled.a`
   padding-top: ${size.xl};
   color: ${white};
   text-decoration: underline;
 `;
+
 const Text = styled.div`
   position: absolute;
   color: white;
   margin-left: 5%;
   margin-top: 10%;
-`;
-
-const Center = styled.div`
-  display: flex;
-  height: 100%;
-  justify-content: center;
-  background-color: #5bbf7d; // Green Color from image
 `;
