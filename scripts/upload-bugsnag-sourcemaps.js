faa44dd45df2fc420b9b81ed61e4df173c943f05
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-10-21 17:27:48 -0400

    Remove console.log

--- scripts/upload-bugsnag-sourcemaps.js
+++ scripts/upload-bugsnag-sourcemaps.js
@@ -1,6 +1,5 @@
 const { browser } = require("@bugsnag/source-maps");
 
-console.log(process.env);
 browser.uploadMultiple({
   apiKey: process.env.REACT_APP_BUGSNAG_API_KEY,
   appVersion: process.env.REACT_APP_VERSION,
