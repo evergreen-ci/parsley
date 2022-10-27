01e4f98bc899dd95c98b00fdbd9b6cb53fe8d7ed
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-10-27 16:04:14 -0400

    Add e2e tests

--- cypress/integration/externalLinks.ts
+++ cypress/integration/externalLinks.ts
@@ -66,6 +66,8 @@ describe("External Links", () => {
     });
   });
   describe("should render links to external pages when viewing a resmoke test log", () => {
+    const taskID =
+      "mongodb_mongo_master_rhel80_debug_v4ubsan_all_feature_flags_experimental_concurrency_sharded_with_stepdowns_and_balancer_4_linux_enterprise_361789ed8a613a2dc0335a821ead0ab6205fbdaa_22_09_21_02_53_24";
     before(() => {
       cy.login();
       cy.visit(
@@ -87,6 +89,22 @@ describe("External Links", () => {
         "have.attr",
         "href",
         "http://localhost:8080/build/7e208050e166b1a9025c817b67eee48d/test/1716e11b4f8a4541c5e2faf70affbfab?html=true"
+      );
+    });
+    it("should render a link to the job logs page", () => {
+      cy.dataCy("job-logs-button").should("be.visible");
+      cy.dataCy("job-logs-button").should(
+        "have.attr",
+        "href",
+        `http://localhost:3000/job-logs/${taskID}/0`
+      );
+    });
+    it("should render a link to the task page", () => {
+      cy.contains("Task Page").should("be.visible");
+      cy.contains("Task Page").should(
+        "have.attr",
+        "href",
+        `http://localhost:9090/task/${taskID}/0?redirect_spruce_users=true`
       );
     });
   });
