3cc6c6558385e276786ffb9713f9f439f59ae1de
minnakt <minna.kt@mongodb.com>
2022-12-05 16:45:35 -0500

    EVG-18295: Remove mock in `storyshots.test.ts`

--- src/storyshots.test.ts
+++ src/storyshots.test.ts
@@ -2,14 +2,6 @@ import initStoryshots, {
   snapshotWithOptions,
 } from "@storybook/addon-storyshots";
 
-// Recent updates to LG components cause a breaking change for storyshots because of the introduction of
-// createUniqueClassName, a function that generates unique classnames at run time. We need to mock out the
-// function so that our snapshots don't break. (https://jira.mongodb.org/browse/PD-2179)
-jest.mock("@leafygreen-ui/lib", () => ({
-  ...jest.requireActual("@leafygreen-ui/lib"),
-  createUniqueClassName: jest.fn().mockReturnValue("static-lg-ui-classname"),
-}));
-
 describe("storyshots", () => {
   // eslint-disable-next-line jest/require-hook
   initStoryshots({
