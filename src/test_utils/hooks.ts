eb17cb1f75ba7784048dfba90294e7d8b2e05e2f
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-12-27 12:13:36 -0500

    Introduce a useEffectDebugger function

---
+++ src/test_utils/hooks.ts
@@ -0,0 +1,56 @@
+import { DependencyList, EffectCallback, useEffect, useRef } from "react";
+
+const usePrevious = <T>(value: T, initialValue: T) => {
+  const ref = useRef(initialValue);
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+/**
+ * `useEffectDebugger` is a custom hook that logs the changed dependencies of a useEffect hook
+ *
+ * @param effectHook - the effect hook
+ * @param dependencies - an array of dependencies
+ * @param dependencyNames - an array of strings that correspond to the dependencies
+ */
+const useEffectDebugger = (
+  effectHook: EffectCallback,
+  dependencies: DependencyList,
+  dependencyNames: string[] = []
+) => {
+  if (process.env.NODE_ENV !== "development") {
+    console.warn(
+      "[use-effect-debugger] This hook should only be used in development!"
+    );
+  }
+  const previousDeps = usePrevious(dependencies, []);
+
+  const changedDeps = dependencies.reduce(
+    (accum: Record<string, { before: any; after: any }>, dependency, index) => {
+      if (dependency !== previousDeps[index]) {
+        const keyName = dependencyNames[index] || index;
+        return {
+          ...accum,
+          [keyName]: {
+            before: previousDeps[index],
+            after: dependency,
+          },
+        };
+      }
+
+      return accum;
+    },
+    {} as Record<string, { before: any; after: any }>
+  );
+
+  if (Object.keys(changedDeps).length) {
+    console.log("[use-effect-debugger] ");
+    console.table(changedDeps);
+  }
+
+  useEffect(effectHook, dependencies);
+};
+
+export { useEffectDebugger };
