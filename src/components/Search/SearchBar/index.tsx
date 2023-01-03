32bfccac54a269c0ac199827a6de6621a8bc40b3
minnakt <47064971+minnakt@users.noreply.github.com>
2023-01-03 15:16:39 -0500

    EVG-18534: Select text content on Ctrl-F shortcut (#156)

--- src/components/Search/SearchBar/index.tsx
+++ src/components/Search/SearchBar/index.tsx
@@ -43,7 +43,8 @@ const SearchBar: React.FC<SearchBarProps> = ({
   useKeyboardShortcut(
     { charKey: CharKey.F, modifierKeys: [ModifierKey.Control] },
     () => {
-      if (inputRef.current) inputRef.current.focus();
+      inputRef.current?.focus();
+      inputRef.current?.select();
     },
     { disabled, ignoreFocus: true }
   );
