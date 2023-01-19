4adf226d1305ca9bf75415631d4cbb28b665c4b9
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-01-19 17:09:19 -0500

    Establish initial scenario

---
+++ memlab/constants/selectors.ts
@@ -0,0 +1,4 @@
+const searchBar = "[data-cy=searchbar-input]";
+const searchCount = "[data-cy=search-count]";
+
+export { searchBar, searchCount };
---
+++ memlab/scenarios/index.ts
@@ -0,0 +1,6 @@
+import { IScenario } from "@memlab/core";
+import performSearch from "./perform-search";
+
+const scenarios: IScenario[] = [performSearch];
+
+export default scenarios;
