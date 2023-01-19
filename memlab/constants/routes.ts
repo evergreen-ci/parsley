330337898ca3a69af2920bd8b96b8cf4960e2ac5
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-01-19 17:08:56 -0500

    Fix base route

--- memlab/constants/routes.ts
+++ memlab/constants/routes.ts
@@ -1,4 +1,4 @@
-const baseURL = "https://localhost:4173";
+const baseURL = "http://localhost:4173";
 
 const resmokeLogPath =
   "/resmoke/7e208050e166b1a9025c817b67eee48d/test/1716e11b4f8a4541c5e2faf70affbfab?bookmarks=0,11079";
