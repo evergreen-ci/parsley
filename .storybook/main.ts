21b994f8838f5d7bb8ddc61603cac6f46a78272c
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-01-27 14:40:31 -0500

    Remove special resolutions for react-test-renderer

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-links",
-  // React 18 causes some issues with storyshots. In package.json we have a resolutions field for
-  // react-test-renderer to get around this. (https://github.com/storybookjs/storybook/issues/17985)
-  "@storybook/addon-storyshots"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-links","@storybook/addon-storyshots"],
   framework: {
     name: "@storybook/react-vite",
     options: {}
