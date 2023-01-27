1f769520706b71c8f2aedb0e57555d16050ae511
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-01-27 14:58:47 -0500

    Actually render the html instead of the js objects

--- src/storyshots.test.ts
+++ src/storyshots.test.ts
@@ -1,21 +1,26 @@
-import initStoryshots, {
-  snapshotWithOptions,
-} from "@storybook/addon-storyshots";
+import initStoryshots from "@storybook/addon-storyshots";
 import { render } from "@testing-library/react";
 
 describe("storyshots", () => {
   // eslint-disable-next-line jest/require-hook
   initStoryshots({
     renderer: render,
-    test: ({ story, context, renderTree, stories2snapsConverter }) => {
+    asyncJest: true,
+    test: ({ story, context, stories2snapsConverter, done }) => {
       const snapshotFileName =
         stories2snapsConverter.getSnapshotFileName(context);
-      return snapshotWithOptions({})({
-        story,
-        context,
-        renderTree,
-        snapshotFileName,
-      });
+      // eslint-disable-next-line testing-library/render-result-naming-convention
+      const component = story.render();
+      const { container } = render(component);
+      // wait until the mount is updated
+      const waitTime = 1;
+      setTimeout(() => {
+        if (snapshotFileName) {
+          expect(container).toMatchSpecificSnapshot(snapshotFileName);
+        }
+
+        done?.();
+      }, waitTime);
     },
   });
 });
