e5d3ee513efa99c02d585f890b5d41a2dc629a16
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-01-27 14:39:16 -0500

    Use common js export

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,14 +10,9 @@ const config: StorybookConfig = {
     name: "@storybook/react-vite",
     options: {}
   },
-  features: {
-    // storyStoreV7 is enabled by default and works well for our general Storybook configuration.
-    // However, we have to disable it because it breaks storyshots. Storybook developers are working
-    // on a new replacement addon for storyshots to fix this in the future.
-    // (https://github.com/storybookjs/storybook/issues/15916)
-    storyStoreV7: false
-  },
   docs: {
     autodocs: true
   }
 };
+
+module.exports = config;
