2a6a364e30ec26c27435612183c6ff9512bb1804
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-01-27 14:46:32 -0500

    Use react testing library render

--- src/storyshots.test.ts
+++ src/storyshots.test.ts
@@ -1,10 +1,12 @@
 import initStoryshots, {
   snapshotWithOptions,
 } from "@storybook/addon-storyshots";
+import { render } from "@testing-library/react";
 
 describe("storyshots", () => {
   // eslint-disable-next-line jest/require-hook
   initStoryshots({
+    renderer: render,
     test: ({ story, context, renderTree, stories2snapsConverter }) => {
       const snapshotFileName =
         stories2snapsConverter.getSnapshotFileName(context);
