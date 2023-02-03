d0d13e29d229df638785208214ec0530e0509894
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-02-03 11:25:26 -0500

    Only Hide console messages in CI

--- config/jest/setupTests.ts
+++ config/jest/setupTests.ts
@@ -1,17 +1,19 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 import "@testing-library/jest-dom/extend-expect";
 
-// Avoid printing debug statements when running tests.
-jest.spyOn(console, "debug").mockImplementation();
+if (process.env.CI) {
+  // Avoid printing debug statements when running tests.
+  jest.spyOn(console, "debug").mockImplementation();
 
-// Avoid printing error statements when running tests.
-jest.spyOn(console, "error").mockImplementation();
+  // Avoid printing error statements when running tests.
+  jest.spyOn(console, "error").mockImplementation();
 
-// Avoid printing analytics events when running tests, but print any other console.log statements.
-const consoleLog = console.log;
-console.log = (message: string) => {
-  if (message.startsWith("ANALYTICS EVENT")) {
-    return;
-  }
-  consoleLog(message);
-};
+  // Avoid printing analytics events when running tests, but print any other console.log statements.
+  const consoleLog = console.log;
+  console.log = (message: string) => {
+    if (message.startsWith("ANALYTICS EVENT")) {
+      return;
+    }
+    consoleLog(message);
+  };
+}
