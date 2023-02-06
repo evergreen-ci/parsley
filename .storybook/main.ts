b0aaed71b3d49ce2d99345f15e519d1e35cf175f
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-02-06 11:20:47 -0500

    Remove unused import

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-import-module-exports
 import type { StorybookConfig } from "@storybook/react-vite";
-import { defineConfig, mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
