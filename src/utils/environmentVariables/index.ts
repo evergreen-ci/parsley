36fe926cbcd0982af20119506e5e900561fefc68
minnakt <minna.kt@mongodb.com>
2023-03-30 15:58:39 -0400

    Fix tests

--- src/utils/environmentVariables/index.ts
+++ src/utils/environmentVariables/index.ts
@@ -1,6 +1,12 @@
 const appVersion = process.env.REACT_APP_VERSION;
 const releaseStage = process.env.REACT_APP_RELEASE_STAGE;
 
+const isLocal = releaseStage === "local";
+const isProduction = releaseStage === "production";
+const isProductionBuild = process.env.NODE_ENV === "production";
+const isStaging = releaseStage === "staging";
+const isDevelopment = () => process.env.NODE_ENV === "development" || isLocal;
+
 const evergreenURL = process.env.REACT_APP_EVERGREEN_URL;
 const graphqlURL = process.env.REACT_APP_GRAPHQL_URL;
 const lobsterURL = process.env.REACT_APP_LOBSTER_URL;
@@ -8,12 +14,6 @@ const logkeeperURL = process.env.REACT_APP_LOGKEEPER_URL;
 const spruceURL = process.env.REACT_APP_SPRUCE_URL;
 
 const bugsnagAPIKey = process.env.REACT_APP_BUGSNAG_API_KEY;
-
-const isDevelopment = () => process.env.NODE_ENV === "development";
-const isLocal = releaseStage === "local";
-const isProduction = releaseStage === "production";
-const isProductionBuild = process.env.NODE_ENV === "production";
-const isStaging = releaseStage === "staging";
 
 export {
   appVersion,
