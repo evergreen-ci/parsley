fc5e6a7388553d4206b84159bd00fb9ef9501d39
minnakt <minna.kt@mongodb.com>
2023-04-04 12:04:29 -0400

    Update lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,9 @@ module.exports = {
   "(?!src/)*.{js,ts,tsx}": ["yarn eslint:staged", "yarn prettier"], // For files that are not in src/, run eslint and prettier
   "src/**/*.{js,ts,tsx}": ["yarn eslint:staged", "yarn prettier"], // For files in src/, run eslint and prettier
   "cypress/**/*.{js,ts}": ["yarn eslint:staged", "yarn prettier"], // For files in cypress/, run eslint and prettier
-  "src/**/*.{graphql}": "yarn prettier --parser graphql", // For GraphQL files, run prettier
+  "src/**/*.{graphql}": [
+    "yarn eslint:staged",
+    "yarn prettier --parser graphql",
+  ], // For GraphQL files, run eslint and prettier
   "*.{ts,tsx}": () => "yarn check-types", // For TypeScript files, check types
 };
