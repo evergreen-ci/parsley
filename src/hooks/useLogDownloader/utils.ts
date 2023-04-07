6751f2a964c6153b6f71e5bc4e7b40daa0f27ca9
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-04-07 11:25:33 -0400

    Add comments to make fetch clearer

--- src/hooks/useLogDownloader/utils.ts
+++ src/hooks/useLogDownloader/utils.ts
@@ -1,7 +1,23 @@
-const fetchStream = async (url: string, abortController?: AbortController) => {
+type Options = {
+  abortController?: AbortController;
+  onProgress?: (progress: number) => void;
+};
+/**
+ * `streamedFetch` is a utility function that exposes a `ReadableStream` from
+ * the response body of a `fetch` request. This allows us to stream the response
+ * body and process it as it comes in, rather than waiting for the entire response.
+ * @param url - the url to fetch
+ * @param options - an object containing options for the fetch request
+ * @param options.abortController - an AbortController that can be used to cancel the request
+ * @param options.onProgress - a callback that will be called with the number of bytes received
+ * @returns a `ReadableStream` from the response body
+ */
+const streamedFetch = async (url: string, options: Options) => {
   const response = await fetch(url, {
     credentials: "include",
-    signal: abortController ? abortController.signal : undefined,
+    signal: options.abortController
+      ? options.abortController.signal
+      : undefined,
   });
 
   if (!response.ok) {
@@ -20,7 +36,7 @@ const fetchStream = async (url: string, abortController?: AbortController) => {
         while (true) {
           // eslint-disable-next-line no-await-in-loop
           const { done, value } = await reader.read();
-
+          options?.onProgress?.(value?.length ?? 0);
           if (done) {
             controller.close();
             break;
@@ -38,8 +54,19 @@ const fetchStream = async (url: string, abortController?: AbortController) => {
   });
 };
 
-const fetchLogFile = async (url: string) => {
-  const stream = await fetchStream(url);
+/**
+ * `fetchLogFile` is a utility function that fetches a log file from the server
+ * and returns it as an array of strings, one for each line. It uses `streamedFetch`
+ * to stream the response body and process it as it comes in. This allows us to
+ * avoid loading the entire log file into memory.
+ * @param url - the url to fetch
+ * @param options - an object containing options for the fetch request
+ * @param options.abortController - an AbortController that can be used to cancel the request
+ * @param options.onProgress - a callback that will be called with the number of bytes received
+ * @returns an array of strings, one for each line in the log file
+ */
+const fetchLogFile = async (url: string, options: Options) => {
+  const stream = await streamedFetch(url, options);
   const decoder = new TextDecoder();
   const reader = stream.getReader();
   const result: string[] = [];
