5bfbfe84a9a13f8a126dc3fe27c6dbba72c306e2
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-04-18 14:30:14 -0400

    Update message copy

--- src/hooks/useLogDownloader/index.ts
+++ src/hooks/useLogDownloader/index.ts
@@ -50,7 +50,7 @@ const useLogDownloader = (
           "error"
         );
         dispatchToast.warning(
-          "We were only able to partially download this log. Use the Evergreen CLI command in the details menu to download the log on to your machine.",
+          "Parsley was only able to partially download this log. Use the Evergreen CLI command in the details menu to download the log onto your machine.",
           true,
           {
             title: "Log not fully downloaded",
