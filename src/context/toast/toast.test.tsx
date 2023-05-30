84fdd7ba15385ac4dca9669eef14f2b65b59a756
Arjun Patel <arjun453@gmail.com>
2023-05-30 13:26:45 -0400

    upgrade toast

--- src/context/toast/toast.test.tsx
+++ src/context/toast/toast.test.tsx
@@ -27,11 +27,17 @@ const wrapper = ({ children }: { children: React.ReactNode }) => (
 );
 
 describe("toast", () => {
+  const closeIconLabel = "Close Message";
   it("should error when rendered outside of ToastProvider context", () => {
+    // This test intentionally throws an error, so we need to mock the error object to prevent it
+    // from flooding the test runner.
+    const errorObject = console.error;
+    jest.spyOn(console, "error").mockImplementation();
     const { Component } = renderComponentWithHook();
     expect(() => render(<Component />)).toThrow(
       "useToastContext must be used within a ToastProvider"
     );
+    console.error = errorObject;
   });
 
   it("should not display a toast by default", () => {
@@ -135,7 +141,7 @@ describe("toast", () => {
       });
 
       expect(screen.getByDataCy("toast")).toBeInTheDocument();
-      await user.click(screen.getByLabelText("X Icon"));
+      await user.click(screen.getByLabelText(closeIconLabel));
       await waitFor(() => {
         expect(screen.queryByDataCy("toast")).not.toBeInTheDocument();
       });
@@ -150,7 +156,7 @@ describe("toast", () => {
         hook.current.info("test string", false);
       });
       expect(screen.getByDataCy("toast")).toBeInTheDocument();
-      expect(screen.queryByLabelText("X Icon")).toBeNull();
+      expect(screen.queryByLabelText(closeIconLabel)).toBeNull();
     });
 
     it("should trigger a callback function onClose", async () => {
@@ -165,7 +171,7 @@ describe("toast", () => {
       });
 
       expect(screen.getByDataCy("toast")).toBeInTheDocument();
-      await user.click(screen.getByLabelText("X Icon"));
+      await user.click(screen.getByLabelText(closeIconLabel));
       await waitFor(() => {
         expect(screen.queryByDataCy("toast")).not.toBeInTheDocument();
       });
@@ -195,24 +201,6 @@ describe("toast", () => {
 
       // Reset to use real timers.
       jest.useRealTimers();
-    });
-
-    it("should close the toast when hide() is called", async () => {
-      const { Component, hook } = renderComponentWithHook();
-      render(<Component />, {
-        wrapper,
-      });
-      act(() => {
-        hook.current.info("test string", true);
-      });
-      expect(screen.getByDataCy("toast")).toBeInTheDocument();
-
-      act(() => {
-        hook.current.hide();
-      });
-      await waitFor(() => {
-        expect(screen.queryByDataCy("toast")).not.toBeInTheDocument();
-      });
     });
   });
 });
