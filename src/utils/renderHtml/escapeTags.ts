f95b7078ac180035684e3e468a18aaf3f3343637
Mohamed Khelif <khelif96@gmail.com>
2023-06-08 10:55:47 -0400

    Update src/utils/renderHtml/escapeTags.ts
    Co-authored-by: minnakt <47064971+minnakt@users.noreply.github.com>

--- src/utils/renderHtml/escapeTags.ts
+++ src/utils/renderHtml/escapeTags.ts
@@ -4,7 +4,7 @@ import xss, { IWhiteList } from "xss";
  * `escapeTags` is a utility function that takes a string with elements that resemble html tags and returns a string that
  * escapes the tags that are not allowed.
  * @param html - the html string to escape
- * @param allowedTags -  a mapping of HTML tags and attributes that should not be escaped
+ * @param allowedTags - a mapping of HTML tags and attributes that should not be escaped
  * @returns The escaped html string
  */
 const escapeTags = (html: string, allowedTags?: IWhiteList) =>
