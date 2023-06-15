08d3cbd10f4d973a0db49f56f2109dad28fba0ad
minnakt <minna.kt@mongodb.com>
2023-06-14 23:46:31 -0400

    society 😔

--- cypress/integration/project_filters.ts
+++ cypress/integration/project_filters.ts
@@ -2,7 +2,7 @@ describe("project filters", () => {
   const spruceLogLink =
     "/evergreen/spruce_ubuntu1604_test_2c9056df66d42fb1908d52eed096750a91f1f089_22_03_02_16_45_12/0/task";
   const resmokeLogLink =
-    "/resmoke/7e208050e166b1a9025c817b67eee48d/test/1716e11b4f8a4541c5e2faf70affbfab";
+    "/resmoke/7e208050e166b1a9025c817b67eee48d/test/1716e17b99558fd9c5e2faf70a00d15d";
 
   beforeEach(() => {
     cy.resetDrawerState();
@@ -11,6 +11,7 @@ describe("project filters", () => {
   it("should show a message if there are no filters", () => {
     cy.visit(spruceLogLink);
     cy.contains("View project filters").click();
+    cy.dataCy("apply-filters-modal").should("be.visible");
     cy.dataCy("project-filter").should("not.exist");
     cy.dataCy("no-filters-message").should("be.visible");
   });
@@ -18,15 +19,22 @@ describe("project filters", () => {
   it("should be able to apply a filter", () => {
     cy.visit(resmokeLogLink);
     cy.contains("View project filters").click();
-    cy.getInputByLabel("D\\d").check({ force: true });
+    cy.dataCy("apply-filters-modal").should("be.visible");
+    cy.getInputByLabel("(NETWORK|ASIO|EXECUTOR|CONNPOOL|REPL_HB)").check({
+      force: true,
+    });
     cy.contains("button", "Apply filters").click();
-    cy.location("search").should("contain", "111D%255Cd");
+    cy.location("search").should(
+      "contain",
+      "111%28NETWORK%257CASIO%257CEXECUTOR%257CCONNPOOL%257CREPL_HB%29"
+    );
     cy.get("[data-cy^='collapsed-row-']").should("exist");
   });
 
   it("should disable checkbox if filter is already applied", () => {
     cy.visit(`${resmokeLogLink}?filters=100D%255Cd`);
     cy.contains("View project filters").click();
+    cy.dataCy("apply-filters-modal").should("be.visible");
     cy.getInputByLabel("D\\d").should("be.disabled");
   });
 });
