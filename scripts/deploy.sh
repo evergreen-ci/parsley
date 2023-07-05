2ed906ed515d0e41607fa87ea9b487d1aa18f785
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-05 16:39:29 -0400

    Add step to write current commit to prod dist

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 # This script runs the aws cli command to deploy the app to s3
 # It also uploads source maps to bugsnag
 
+# Create a file in dist/ that contains the current commit hash
+CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
+echo $CURRENT_COMMIT_HASH > dist/commit.txt
+
 # Try this step and throw an error if it fails
 echo "Deploying to S3"
 aws s3 sync dist/ s3://${BUCKET}/ --acl public-read --follow-symlinks --delete --exclude .env-cmdrc.json 
