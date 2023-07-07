be6d4497861492d34adc56dd90c754690c5fd259
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-07 17:43:31 -0400

    Shellcheck

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # This script runs the aws cli command to deploy the app to s3
 # It also uploads source maps to bugsnag
 
@@ -5,11 +7,11 @@ REACT_APP_PARSLEY_URL=$REACT_APP_PARSLEY_URL sh scripts/get-current-deployed-com
 
 # Create a file in dist/ that contains the current commit hash
 CURRENT_COMMIT_HASH=$(git rev-parse HEAD)
-echo $CURRENT_COMMIT_HASH > dist/commit.txt
+echo "$CURRENT_COMMIT_HASH" > dist/commit.txt
 
 # # Try this step and throw an error if it fails
 echo "Deploying to S3"
-aws s3 sync dist/ s3://${BUCKET}/ --acl public-read --follow-symlinks --delete --exclude .env-cmdrc.json 
+aws s3 sync dist/ s3://"${BUCKET}"/ --acl public-read --follow-symlinks --delete --exclude .env-cmdrc.json 
 echo "Deployed to S3"
 
 # If the above step succeeds, run this step
