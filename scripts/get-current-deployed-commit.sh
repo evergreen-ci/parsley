2f9ed219966c48d4c7ada6ea0c9387abd0cf548f
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-07-12 16:49:06 -0400

    Fix order

--- scripts/get-current-deployed-commit.sh
+++ scripts/get-current-deployed-commit.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 BASE_URL=$REACT_APP_PARSLEY_URL
+if [ "$BASE_URL" == "" ]
+then
+    echo "BASE_URL not set"
+    exit 1
+fi
+
 # First download the currently deployed commit hash from s3
+# If the curl succeeds, and is exactly 40 characters, then use that commit hash
 # If the curl fails, then use the previous tag from git
-# If the curl succeeds, then use the githash from the curl
-
-
-# Bash check for error 
-
 PREVIOUS_DEPLOYED_COMMIT=$(curl -s "$BASE_URL"/commit.txt)
 # If the curl is more than 40 characters, then it's not a commit hash
 if [ ${#PREVIOUS_DEPLOYED_COMMIT} -gt 40 ]
@@ -15,12 +17,6 @@ then
     echo "curl failed"
     echo "Using previous tag from git"
     PREVIOUS_DEPLOYED_COMMIT=$(git describe --tags --abbrev=0)
-fi
-
-if [ "$BASE_URL" == "" ]
-then
-    echo "BASE_URL not set"
-    exit 1
 fi
 
 if [ "$PREVIOUS_DEPLOYED_COMMIT" == "" ]
