33a4b2c3ce4eb0e3c52c7ee83a7056751ca34f8e
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-08-02 17:19:52 -0400

    Copy over spruce deploy script

--- scripts/deploy/upload-bugsnag-sourcemaps.js
+++ scripts/deploy/upload-bugsnag-sourcemaps.js
@@ -4,6 +4,6 @@ browser.uploadMultiple({
   apiKey: process.env.REACT_APP_BUGSNAG_API_KEY,
   appVersion: process.env.REACT_APP_VERSION,
   overwrite: true,
-  directory: "./dist/assets",
+  directory: "./build/assets",
   baseUrl: `${process.env.REACT_APP_PARSLEY_URL}/assets`,
 });
