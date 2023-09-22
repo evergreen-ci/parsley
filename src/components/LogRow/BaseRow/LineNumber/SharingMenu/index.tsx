3f42be2324ef292035acc23b4ee107ec820272f6
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-09-22 10:44:15 -0400

    Wire up Sharing Menu options for copy and range search

--- src/components/LogRow/BaseRow/LineNumber/SharingMenu/index.tsx
+++ src/components/LogRow/BaseRow/LineNumber/SharingMenu/index.tsx
@@ -1,18 +1,51 @@
 import { Menu, MenuItem } from "@leafygreen-ui/menu";
+import { QueryParams } from "constants/queryParams";
+import { useLogContext } from "context/LogContext";
 import { useMultiLineSelectContext } from "context/MultiLineSelectContext";
+import { useToastContext } from "context/toast";
+import { useQueryParam } from "hooks/useQueryParam";
+import { copyToClipboard, getJiraFormat } from "utils/string";
 
 interface SharingMenuProps {
   open: boolean;
   refEl: React.RefObject<HTMLElement>;
 }
 const SharingMenu: React.FC<SharingMenuProps> = ({ open, refEl }) => {
-  const { handleCloseMenu } = useMultiLineSelectContext();
+  const { handleCloseMenu, selectedLines } = useMultiLineSelectContext();
+  const { getLine } = useLogContext();
+  const [, setLowerRange] = useQueryParam(QueryParams.LowerRange, 0);
+  const [, setUpperRange] = useQueryParam<undefined | number>(
+    QueryParams.UpperRange,
+    undefined
+  );
+  const dispatchToast = useToastContext();
+
+  const handleCopySelectedLines = async () => {
+    const { endingLine, startingLine } = selectedLines;
+    if (startingLine === null || endingLine === null) return;
+    // Create an array of line numbers that represent the range in selectedLines
+    const bookmarks = Array.from(
+      { length: endingLine - startingLine + 1 },
+      (_, i) => i + startingLine
+    );
+    await copyToClipboard(getJiraFormat(bookmarks, getLine));
+    dispatchToast.success(`Copied ${bookmarks.length} lines to clipboard`);
+  };
+
+  const handleOnlySearchOnRange = () => {
+    const { endingLine, startingLine } = selectedLines;
+    if (startingLine === null || endingLine === null) return;
+    setLowerRange(startingLine);
+    setUpperRange(endingLine);
+  };
 
   return (
     <Menu open={open} refEl={refEl} setOpen={handleCloseMenu}>
-      <MenuItem>Copy selected lines</MenuItem>
+      <MenuItem onClick={handleCopySelectedLines}>Copy selected lines</MenuItem>
       <MenuItem>Share selected link</MenuItem>
-      <MenuItem>Only search on range</MenuItem>
+      <MenuItem onClick={handleOnlySearchOnRange}>
+        Only search on range
+      </MenuItem>
     </Menu>
   );
 };
