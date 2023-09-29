80c500ddd8d037cd2e850497398529c3778110d9
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-09-29 16:24:33 -0400

    Surface file name on task subheader

---
+++ src/components/SubHeader/SubHeader.stories.tsx
@@ -0,0 +1,81 @@
+import { useEffect } from "react";
+import { MockedProvider } from "@apollo/client/testing";
+import { LogTypes } from "constants/enums";
+import { useLogContext } from "context/LogContext";
+import { LogMetadata } from "context/LogContext/types";
+import { evergreenTaskMock, logkeeperMetadataMock } from "test_data/task";
+import { CustomMeta, CustomStoryObj } from "test_utils/types";
+import Subheader from ".";
+
+export default {
+  component: Subheader,
+  decorators: [
+    (Story: () => JSX.Element) => (
+      <MockedProvider mocks={[evergreenTaskMock, logkeeperMetadataMock]}>
+        <Story />
+      </MockedProvider>
+    ),
+  ],
+} satisfies CustomMeta<typeof Subheader>;
+
+export const UploadedLog: CustomStoryObj<typeof SubheaderWrapper> = {
+  argTypes: {},
+  args: {
+    fileName: "some-file-name.log",
+    isUploadedLog: true,
+  },
+  render: (args) => <SubheaderWrapper {...args} />,
+};
+
+export const TaskLog: CustomStoryObj<typeof SubheaderWrapper> = {
+  argTypes: {},
+  args: {
+    execution: "0",
+    isUploadedLog: false,
+    logType: LogTypes.EVERGREEN_TASK_LOGS,
+    taskID:
+      "spruce_ubuntu1604_check_codegen_d54e2c6ede60e004c48d3c4d996c59579c7bbd1f_22_03_02_15_41_35",
+  },
+  render: (args) => <SubheaderWrapper {...args} />,
+};
+
+export const TestLog: CustomStoryObj<typeof SubheaderWrapper> = {
+  argTypes: {},
+  args: {
+    execution: "0",
+    isUploadedLog: false,
+    logType: LogTypes.EVERGREEN_TEST_LOGS,
+    taskID:
+      "spruce_ubuntu1604_check_codegen_d54e2c6ede60e004c48d3c4d996c59579c7bbd1f_22_03_02_15_41_35",
+    testID: "JustAFakeTestInALonelyWorld",
+  },
+  render: (args) => <SubheaderWrapper {...args} />,
+};
+
+export const TaskFileLog: CustomStoryObj<typeof SubheaderWrapper> = {
+  argTypes: {},
+  args: {
+    execution: "0",
+    fileName: "some-file-name.log",
+    isUploadedLog: false,
+    logType: LogTypes.EVERGREEN_TEST_LOGS,
+    taskID:
+      "spruce_ubuntu1604_check_codegen_d54e2c6ede60e004c48d3c4d996c59579c7bbd1f_22_03_02_15_41_35",
+  },
+  render: (args) => <SubheaderWrapper {...args} />,
+};
+interface SubheaderWrapperProps extends LogMetadata {
+  isUploadedLog: boolean;
+}
+
+const SubheaderWrapper: React.FC<SubheaderWrapperProps> = ({
+  isUploadedLog,
+  ...metaData
+}) => {
+  const { setLogMetadata } = useLogContext();
+
+  useEffect(() => {
+    setLogMetadata(metaData);
+  }, []);
+  return <Subheader isUploadedLog={isUploadedLog} />;
+};
