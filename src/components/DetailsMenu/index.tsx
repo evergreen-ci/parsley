3cb7f70dbd5d60bad195d047212aea30ec438c3a
Mohamed Khelif <mohamed.khelif@mongodb.com>
2023-10-31 15:46:17 -0400

    Details menu should animate when field is toggled

--- src/components/DetailsMenu/index.tsx
+++ src/components/DetailsMenu/index.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
 import PopoverButton from "components/PopoverButton";
+import { QueryParams } from "constants/queryParams";
+import { useQueryParam } from "hooks/useQueryParam";
 import DetailsMenuCard from "./DetailsMenuCard";
 
 interface DetailsMenuProps {
   disabled?: boolean;
 }
-const DetailsMenu: React.FC<DetailsMenuProps> = ({ disabled, ...rest }) => (
-  <PopoverButton buttonText="Details" disabled={disabled} {...rest}>
-    <DetailsMenuCard data-cy="details-menu" />
-  </PopoverButton>
-);
+const DetailsMenu: React.FC<DetailsMenuProps> = ({ disabled, ...rest }) => {
+  const [lowerRange] = useQueryParam(QueryParams.LowerRange, 0);
+  const [upperRange] = useQueryParam<undefined | number>(
+    QueryParams.UpperRange,
+    undefined
+  );
+  const [changeVisible, setChangeVisible] = useState(false);
+
+  useEffect(() => {
+    setChangeVisible(true);
+    const timer = setTimeout(() => {
+      setChangeVisible(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [lowerRange, upperRange]);
+
+  return (
+    <PopoverButton
+      buttonText="Details"
+      disabled={disabled}
+      variant={changeVisible ? "primary" : "default"}
+      {...rest}
+    >
+      <DetailsMenuCard data-cy="details-menu" />
+    </PopoverButton>
+  );
+};
 
 export default DetailsMenu;
