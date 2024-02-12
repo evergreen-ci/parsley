9ea9e96f072db009631edf3b4b3f34b5e55e7dbc
Arjun Patel <arjun453@gmail.com>
2024-02-12 15:07:22 -0500

    rename gql file

--- src/gql/queries/index.ts
+++ src/gql/queries/index.ts
@@ -1,6 +1,6 @@
 import GET_LOGKEEPER_TASK from "./get-logkeeper-task.graphql";
 import GET_TASK from "./get-task.graphql";
-import GET_TEST_LOG_URL from "./get-test-log-url.graphql";
+import GET_TEST_LOG_URL from "./get-test-log-url-and-rendering-type.graphql";
 import GET_USER from "./get-user.graphql";
 import PROJECT_FILTERS from "./project-filters.graphql";
 import TASK_FILES from "./task-files.graphql";
