04ef0698f95f9c4c0b8d2fc15d6d92d241600acd
Mohamed Khelif <mohamed.khelif@mongodb.com>
2024-03-05 15:17:26 -0500

    Add "Pasted text" action to useLogDropAnalytics

--- src/analytics/logDrop/useLogDropAnalytics.ts
+++ src/analytics/logDrop/useLogDropAnalytics.ts
@@ -5,6 +5,7 @@ type Action =
   | { name: "Clicked Upload Link"; hasLogs: boolean }
   | { name: "Dropped file" }
   | { name: "Uploaded Log File With Picker" }
+  | { name: "Pasted text" }
   | { name: "Processed Log"; logType: LogTypes; fileSize?: number };
 
 export const useLogDropAnalytics = () => useAnalyticsRoot<Action>("LogDrop");
